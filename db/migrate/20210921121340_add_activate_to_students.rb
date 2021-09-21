eb5c9313de863ff16466dba2530b71f26f9e580c
Cooper Sawyer <cooper.sawyer@powerhrg.com>
2021-09-21 09:10:01 -0400

    finished lab

---
+++ db/migrate/20210921121340_add_activate_to_students.rb
@@ -0,0 +1,5 @@
+class AddActivateToStudents < ActiveRecord::Migration[5.0]
+  def change
+    add_column :students, :active, :boolean, default: false
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Student.create(first_name: "steve", last_name: "bonjovy", active: 1 )
